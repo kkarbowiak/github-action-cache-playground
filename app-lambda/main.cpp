1a42e26ff2ad9c6ee7b8bcd93c8bfd855bd6f3c9
Krzysiek Karbowiak <krzysztof.karbowiak@interia.pl>
2023-01-05 09:31:36 +0100

    Comment unused parameter

--- app-lambda/main.cpp
+++ app-lambda/main.cpp
@@ -1,7 +1,7 @@
 #include <aws/lambda-runtime/runtime.h>
 
 
-auto handler(aws::lambda_runtime::invocation_request const & request) -> aws::lambda_runtime::invocation_response
+auto handler(aws::lambda_runtime::invocation_request const & /* request */) -> aws::lambda_runtime::invocation_response
 {
     return aws::lambda_runtime::invocation_response::success(R"({"result":"success","size\:"great"})", "application/json");
 }
